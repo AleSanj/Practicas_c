8b9cf6593de178ba01efb497cbd988d6231e8dcc
Alesanj <alejandro.sanjinez1@gmail.com>
2021-03-21 13:34:14 -0300

    el array concat[] ya tiene los datos correctos de concatenacion el problema ahora es entre la conversion de char[] y char*, el prototipo de la funcion nos pide un char* pero se trabajo todo el tiempo con char[]

--- Ejercicio1/main.c
+++ Ejercicio1/main.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
 
 char* string_concat(char*, char*);
 
 
 int main () {
-    char* a ="hola";
-    char* b = "uno";
+    char a[] ="hola";
+    char b[] = "uno";
 
-    printf("concatencacion: %s \n",string_concat(a,b));
+//    char* final = string_concat(a,b);
+    printf("concatencacion: %s \n",(char*) string_concat(a,b));
 
 	return 0;
 }
@@ -20,20 +21,26 @@ char* string_concat(char* primero, char* segundo){
 
 	int tamanio = tamanioPrim + tamanioSeg;
     printf("%d y %d y %d\n",tamanioPrim,tamanioSeg, tamanio);
-	char* concat[tamanio];
+	char concat[tamanio];
 
 
 	for(int i=0;i<tamanio;i++){
+
     if (i < tamanioPrim){
 	printf("%c\n",primero[i]);
 	concat[i] = primero[i];
 	    }else {
-	        printf("%c\n",segundo[i-tamanioPrim]);
-	        concat[i] = 'a';
+	    	printf("%c\n",segundo[i-tamanioPrim]);
+	    	concat[i] = segundo[i-tamanioPrim];
+
 	    }
 
-
-
 	}
+
+
 	concat[tamanio] = '\0';
+
+	return concat;
+
+
 }
