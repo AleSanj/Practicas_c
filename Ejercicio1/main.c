8dfcd781d675c3a8248e884df07a37e7df74ba69
Alesanj <alejandro.sanjinez1@gmail.com>
2021-03-23 16:43:17 -0300

    encarada la parte 3 hay un problema al tratar de copiar caracter a caracter el usuario o el dominio

--- Ejercicio1/main.c
+++ Ejercicio1/main.c
@@ -4,7 +4,7 @@
 
 char* string_concat(char*, char*);
 void string_concat_dinamyc(char*,char*, char* (*funcion) (char*,char*));
-
+void mail_split(char*, char**, char**);
 
 int main () {
 
@@ -14,7 +14,16 @@ int main () {
     printf("concatencacion: %s \n",string_concat(a,b));
 
     string_concat_dinamyc(a,b,&string_concat);
-	return 0;
+
+    char* user;
+    char* dominio;
+    char* mail = "ale@gmail.com";
+
+    mail_split(mail,&user,&dominio);
+
+    printf("user: %s\ndominio: %s\n",user,dominio);
+
+    return 0;
 }
 
 char* string_concat(char* primero, char* segundo){
@@ -43,7 +52,26 @@ char* string_concat(char* primero, char* segundo){
 }
 
 void string_concat_dinamyc(char* primero,char* segundo, char* (*funcion) (char*,char*)){
-	printf("concatenacion2: %s",funcion(primero,segundo));
+	printf("concatenacion2: %s\n",funcion(primero,segundo));
 
 }
+
+void mail_split(char* mail, char** user, char** dominio){
+	int tamanio = strlen(mail);
+	int flag = 1;
+	char* usuario[4];
+	for (int i=0; i< tamanio;i++){
+		if (mail[i]!='@' && flag){
+			usuario[i] = mail[i];
+		} else if(mail[i]=='@') {
+			flag=0;
+
+		} else {
+//			dominio[i-strlen(user)] = mail[i];
+		}
+	}
+	printf("u:%s \n",usuario);
+}
+
+
 
